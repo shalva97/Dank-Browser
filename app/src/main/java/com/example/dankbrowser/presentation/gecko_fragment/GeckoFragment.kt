af377f30f9f024915d08052acf5a47507fdc48dd
shalva97 <shalva258@gmail.com>
2021-10-27 23:45:31 +0400

    add live data

--- app/src/main/java/com/example/dankbrowser/presentation/gecko_fragment/GeckoFragment.kt
+++ app/src/main/java/com/example/dankbrowser/presentation/gecko_fragment/GeckoFragment.kt
@@ -5,14 +5,8 @@ import android.view.View
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.example.dankbrowser.R
 import com.example.dankbrowser.databinding.FragmentGeckoBinding
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 import org.mozilla.geckoview.GeckoView
 
 class GeckoFragment : Fragment(R.layout.fragment_gecko) {
@@ -36,18 +30,14 @@ class GeckoFragment : Fragment(R.layout.fragment_gecko) {
             viewModel.hideUrlBar()
         }
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.CREATED) {
-                viewModel.selectedTask.onEach {
-                    if (browserGV.session != it.selectedTab.getSession()) {
-                        browserGV.setSession(it.selectedTab.getSession())
-                    }
-                }.launchIn(this)
-
-                viewModel.urlBar.onEach {
-                    urlBarEWC.isVisible = it
-                }.launchIn(this)
+        viewModel.selectedTask.observe(viewLifecycleOwner) {
+            if (browserGV.session != it.selectedTab.getSession()) {
+                browserGV.setSession(it.selectedTab.getSession())
             }
+        }
+
+        viewModel.urlBar.observe(viewLifecycleOwner) {
+            urlBarEWC.isVisible = it
         }
     }
 
--- app/src/main/java/com/example/dankbrowser/presentation/gecko_fragment/GeckoViewModel.kt
+++ app/src/main/java/com/example/dankbrowser/presentation/gecko_fragment/GeckoViewModel.kt
@@ -2,14 +2,12 @@ package com.example.dankbrowser.presentation.gecko_fragment
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asLiveData
 import com.example.dankbrowser.DankApplication
 import com.example.dankbrowser.components
 import com.example.dankbrowser.domain.TaskList
 import com.example.dankbrowser.domain.Url
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import org.mozilla.geckoview.GeckoRuntime
 import org.mozilla.geckoview.GeckoSession
 
@@ -19,20 +17,20 @@ class GeckoViewModel(
 
     private val taskList: TaskList = components.taskList
     private val geckoRuntime: GeckoRuntime = (application as DankApplication).components.geckoEngine
-    val selectedTask = taskList.getSelectedTab()
-    val urlBar = MutableSharedFlow<Boolean>(1, 1)
+    val selectedTask = taskList.getSelectedTab().asLiveData()
+    val urlBar = MutableLiveData<Boolean>()
 
     init {
-        selectedTask.onEach { task ->
+        selectedTask.observeForever { task ->
             if (task.selectedTab.url is Url.Empty) {
-                urlBar.tryEmit(true)
+                urlBar.postValue(true)
             } else {
                 task.selectedTab.loadWebsite(geckoRuntime)
                 hideUrlBar()
             }
 
             task.selectedTab.geckoSession.navigationDelegate = navigationDelegate()
-        }.launchIn(viewModelScope)
+        }
     }
 
     private fun navigationDelegate(): GeckoSession.NavigationDelegate {
@@ -47,12 +45,12 @@ class GeckoViewModel(
     }
 
     fun changeUrl(url: String) {
-        val task = selectedTask.replayCache.first()
-        taskList.changeUrl(task.selectedTab, url, task)
+        val task = selectedTask.value
+        taskList.changeUrl(task!!.selectedTab, url, task)
     }
 
     fun hideUrlBar() {
-        urlBar.tryEmit(false)
+        urlBar.postValue(false)
     }
 
 }
